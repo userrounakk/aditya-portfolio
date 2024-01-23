6fa6ae9e8d2397c44f9512558b71744799be2e98
Aditya <aditya.chaudhary1558@gmail.com>
2024-01-23 15:21:08 +0530

    trying to fix the error when going for vercel deployment

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({props}) => {
+const Button = ({props}: any) => {
   return (
     <button className='bg-white py-[0.5rem] px-[1rem] rounded-2xl'>
         {props}
